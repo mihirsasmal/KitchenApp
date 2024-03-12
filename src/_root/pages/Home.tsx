b32318737ec46522b82f22c5365c4d7d3ae7a11a
mihirsasmal <mihirsasmal@gmail.com>
2024-03-12 12:40:57 +0000

    ui alignment of Home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,7 +1,6 @@
 import Loader from '@/components/shared/Loader'
 import RecipeCard from '@/components/shared/RecipeCard';
 import { useGetRecentRecipeMutation } from '@/lib/react-query/queriesAndMutation';
-import { IRecipe } from '@/types';
 import { Models } from 'appwrite';
 
 const Home = () => {
@@ -9,13 +8,13 @@ const Home = () => {
   const {data:recipes, isPending:isRecipeLoading, isError:isErrorRecipes} = useGetRecentRecipeMutation();
   return (
     <div className='flex flex-1'>
-      <div className='flex flex-col flex-1 items-center gap-10 overflow-scroll py-10 px-5 md:px-8 lg:p-14 custom-scrollbar-Container'>
-        <div className='max-w-screen-sm flex flex-col items-center w-full gap-6 md:gap-9'>
-          <h2 className='h3-bold md:h2-bold text-left w-full'> Home Feed</h2>
+      <div className='flex flex-col flex-1 items-center gap-10 overflow-scroll py-10 px-5 md:px-8 lg:p-14 custom-scrollbar'>
+        <div className='max-w-5xl flex-start flex-col gap-3 justify-start w-full'>
+          <h2 className='h3-bold md:h2-bold text-left w-full'> Home</h2>
           {isRecipeLoading && !recipes ?(
             <Loader/>
           ) : (
-            <ul className='flex flex-col flex-1 gap-9 w-full'>
+            <ul className='sm:w-full flex-start flex-col gap-9 w-full'>
               {(recipes as any)?.documents.map((recipe:Models.Document)=>(
                 <RecipeCard recipe={recipe} key={recipe.$id}/>
               ))}
