78b7213a7264408c1d93670abc2a7de8d2589de2
mihirsasmal <mihirsasmal@gmail.com>
2024-03-19 21:34:07 +0000

    Recipe Card Ui updated

--- src/components/shared/RecipeCard.tsx
+++ src/components/shared/RecipeCard.tsx
@@ -1,9 +1,9 @@
 import { useUserContext } from '@/context/AuthContext';
 import { formatDate } from '@/lib/utils';
 import { Models } from 'appwrite';
-import React from 'react'
 import { Link } from 'react-router-dom';
 import RecipeStats from './RecipeStats';
+import RecipeActions from './RecipeActions';
 
 type RecipeCardProps = {
     recipe:Models.Document;
@@ -11,32 +11,28 @@ type RecipeCardProps = {
 const RecipeCard = ({recipe}:RecipeCardProps) => {
 const {user} = useUserContext();
 
-//if(!recipe.creator) return;
 
   return (
     <div className='bg-dark-2 rounded-3xl border border-dark-4 p-5 lg:p-7 w-full max-w-screen-sm;'>
         <div className = 'flex flex-col gap-5 justify-between items-center'>
             <div className = 'flex items-center gap-3'>
-                <Link to={`/profile/${recipe.creator.$id}`}>
+                <Link to={`/recipe/${recipe.$id}`}>
                     <img 
                     src = {recipe.ImageUrl || 'assets/icons/profile-placeholder.svg'}
-                    alt = 'creator'
-                className='rounded-full w-12 lg:h-12'
+                    alt = 'Recipe Image'
+                className='aspect-auto w-32 lg:h-32'
             />
                 </Link>
-                <div className='flex flex-col'>
+                <Link to={`/recipe/${recipe.$id}`}>
+                <div className='flex-col gap-5'>
                     <p className='base-medium lg:body-bold text-light-1'> {recipe.RecipeName}</p>
                     <div className='flex-center gap-2 text-light-3'>
-                    <p className='subtle-semibold lg:small-regular'>by {recipe.creator.name}</p> - 
+                    <p className='subtle-semibold lg:small-regular'>by {recipe.creator.name} </p> - 
                         <p className='subtle-semibold lg:small-regular'>{formatDate(recipe.$createdAt)}</p>
                     </div>
                 </div>
-                {(user.id ===recipe.creator.$id) ? 
-                (
-                <Link to={`/update-recipe/${recipe.$id}`}>
-                <img src = '/assets/icons/edit.svg' alt = 'edit' width={20} height = {20}/>                
-            </Link>
-            ):( <></>)}
+                </Link>
+                {<RecipeActions recipe = {recipe} userId = {user.id}/>}
             </div>
            
             <RecipeStats recipe={recipe} userId={user.id}/>
