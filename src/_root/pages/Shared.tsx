6062f6b07f1d3b9db9252223853b109d4d597d5f
mihirsasmal <mihirsasmal@gmail.com>
2024-04-25 12:04:28 +0100

    tidy up

--- src/_root/pages/Shared.tsx
+++ src/_root/pages/Shared.tsx
@@ -1,6 +1,6 @@
 import Loader from '@/components/shared/Loader';
 import { useUserContext } from '@/context/AuthContext';
-import { useGetRecipeByUserMutation, useGetSharedRecipeOfUserMutation } from '@/lib/react-query/queriesAndMutation';
+import { useGetSharedRecipeOfUserMutation } from '@/lib/react-query/queriesAndMutation';
 import { useEffect, useState } from 'react'
 import {
   DropdownMenu,
